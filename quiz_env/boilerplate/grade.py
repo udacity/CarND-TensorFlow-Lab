1121ce70e8dd3da2c16c68330c6f32e1a30c9088
Brok Bucholtz <brok.bucholtz@gmail.com>
2016-10-20 16:53:28 -0700

    Handle Syntax Errors

--- quiz_env/boilerplate/grade.py
+++ quiz_env/boilerplate/grade.py
@@ -1,4 +1,12 @@
 import json
 
-result = json.loads(executor_result['stdout'])
+
+try:
+    result = json.loads(executor_result['stdout'])
+except:
+    result = {
+        'correct': False,
+        'feedback': 'There\'s an error with the code',
+        'comment': 'Click "Test Run" to see the error message'}
+
 grade_result.update(result)
