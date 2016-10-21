63dc3003ce7730ca3cc231a87ba5a36f22352b47
Brok Bucholtz <brok.bucholtz@gmail.com>
2016-10-20 17:18:51 -0700

    Add cases to softmax quiz

--- quiz_env/d_tensorflow_softmax/quiz_test.py
+++ quiz_env/d_tensorflow_softmax/quiz_test.py
@@ -1,4 +1,5 @@
 import numpy as np
+from tensorflow.python.framework.errors import InvalidArgumentError
 
 
 def get_result(student_func):
@@ -11,13 +12,22 @@ def get_result(student_func):
         'feedback': 'That\'s the wrong answer.  It should print {}'.format(answer),
         'comment': ''}
 
-    output = student_func()
-    if not isinstance(output, np.ndarray):
-        result['feedback'] = 'Output is the wrong type.'
-        result['comment'] = 'The output should come from running the session.'
-    elif np.allclose(output, answer):
-        result['correct'] = True
-        result['feedback'] = 'That\'s the correcct softmax!'
-
+    try:
+        output = student_func()
+        if not isinstance(output, np.ndarray):
+            result['feedback'] = 'Output is the wrong type.'
+            result['comment'] = 'The output should come from running the session.'
+        elif np.allclose(output, [2, 1, 0.1]):
+            result['feedback'] = 'You\'re returning the logits.'
+            result['comment'] = 'You need to apply softmax to the logits and return them.'
+        elif np.allclose(output, answer):
+            result['correct'] = True
+            result['feedback'] = 'That\'s the correcct softmax!'
+    except InvalidArgumentError as err:
+        if err.message.startswith('You must feed a value for placeholder tensor'):
+            result['feedback'] = 'The placeholder is not being set.'
+            result['comment'] = 'Try using the feed_dict.'
+        else:
+            raise
 
     return result
