dca98d0c00bf01ba5c0121356ba3e4aad36e592f
Brok Bucholtz <brok.bucholtz@gmail.com>
2016-10-20 17:29:13 -0700

    Add case to TensorFlow cross entropy quiz

--- quiz_env/e_tensorflow_cross_entropy/quiz_test.py
+++ quiz_env/e_tensorflow_cross_entropy/quiz_test.py
@@ -1,4 +1,5 @@
 import numpy as np
+from tensorflow.python.framework.errors import InvalidArgumentError
 
 
 def get_result(student_func):
@@ -11,12 +12,19 @@ def get_result(student_func):
         'feedback': 'That\'s the wrong answer.  It should print {}'.format(answer),
         'comment': ''}
 
-    output = student_func()
-    if not isinstance(output, np.float32):
-        result['feedback'] = 'Output is the wrong type.'
-        result['comment'] = 'The output should come from running the session.'
-    elif np.allclose(output, answer):
-        result['correct'] = True
-        result['feedback'] = 'Your Cross Entropy function is correct'
+    try:
+        output = student_func()
+        if not isinstance(output, np.float32):
+            result['feedback'] = 'Output is the wrong type.'
+            result['comment'] = 'The output should come from running the session.'
+        elif np.allclose(output, answer):
+            result['correct'] = True
+            result['feedback'] = 'Your Cross Entropy function is correct'
+    except InvalidArgumentError as err:
+        if err.message.startswith('You must feed a value for placeholder tensor'):
+            result['feedback'] = 'The placeholder is not being set.'
+            result['comment'] = 'Try using the feed_dict.'
+        else:
+            raise
 
     return result
